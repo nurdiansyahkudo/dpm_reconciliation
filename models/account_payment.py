83989da7a7e91c3905e8a4a16110459732374d0b
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-08 08:42:22 +0700

    Update action post in account payment

--- models/account_payment.py
+++ models/account_payment.py
@@ -14,21 +14,21 @@ class AccountPayment(models.Model):
 
             bank_journal = payment.journal_id
 
-            # Cari atau buat bank statement yang masih draft
-            statement = self.env['account.bank.statement'].search([
-                ('journal_id', '=', bank_journal.id),
-                ('date', '=', payment.date),
-            ], limit=1)
+            #  Cari atau buat bank statement yang masih draft
+            # statement = self.env['account.bank.statement'].search([
+            #     ('journal_id', '=', bank_journal.id),
+            #     ('date', '=', payment.date),
+            # ], limit=1)
 
-            if not statement:
-                statement = self.env['account.bank.statement'].create({
-                    'journal_id': bank_journal.id,
-                    'date': payment.date,
-                })
+            # if not statement:
+            #     statement = self.env['account.bank.statement'].create({
+            #         'journal_id': bank_journal.id,
+            #         'date': payment.date,
+            #     })
 
             # Buat baris bank statement
             self.env['account.bank.statement.line'].create({
-                'statement_id': statement.id,
+                # 'statement_id': statement.id,
                 'payment_ref': payment.ref or payment.name,
                 'partner_id': payment.partner_id.id,
                 'amount': payment.amount if payment.payment_type in ['inbound', 'transfer'] else -payment.amount,
